3194c8734e93564760bca0978540b7d0c933140e
Victor Jose Garcia Ferrnandez <victorjgf@gmail.com>
2018-12-09 17:21:32 +0100

    Fix errors in inputData

--- src/inputData.py
+++ src/inputData.py
@@ -1,12 +1,16 @@
 import numpy as np
+
 def inputData(path):
+	x = y = np.array([])
 	with open(path) as f:
 		content = [(float(x.strip().split()[0]),float(x.strip().split()[1])) for x in f.readlines()]
+	
+		x = np.array([x[0] for x in content])
+		y = np.array([x[1] for x in content])
+		maxVal = max(np.concatenate((x,y)))
+		x = x/maxVal
+		y = y/maxVal
 
-	x = np.array([x[0] for x in content])
-	y = np.array([x[1] for x in content])
-	maxVal = max(np.concatenate((x,y)))
-	x = x/maxVal
-	y = y/maxVal
-return x,y
+	return (x,y)
+
 #NVAR=len(x)
