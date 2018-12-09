181998ba547f6c4f4fa0ae1413af5bef76e9526e
Victor Jose Garcia Ferrnandez <victorjgf@gmail.com>
2018-12-09 16:00:55 +0100

    Change spaces for tabs

--- src/select.py
+++ src/select.py
@@ -5,33 +5,30 @@ import numpy as np
 # for the actual selection process.
 
 def select(SEL_F, Chrom, FitnV, GGAP = 1.0, SUBPOP = 1):
-    #Identify the population size (Nind)
-    #Chrom is a matrix
-    #FitnV is an array
-    NindCh,Nvar = Chrom.shape
-    NindF = len(FitnV)
+	#Identify the population size (Nind)
+	#Chrom is a matrix
+	#FitnV is an array
+	NindCh,Nvar = Chrom.shape
+	NindF = len(FitnV)
 
-    if (NindCh != NindF):
-        raise disagree('Chrom and FitnV disagree')
-    if (GGAP < 0):
-        error('GGAP must be a scalar bigger than 0'); end
-    if ((NindCh/SUBPOP) != np.fix(NindCh/SUBPOP)).all():
-        raise disagree('Chrom and SUBPOP disagree')
-    Nind = NindCh/SUBPOP  #Compute number of individuals per subpopulation
+	if (NindCh != NindF):
+		raise disagree('Chrom and FitnV disagree')
+	if (GGAP < 0):
+		error('GGAP must be a scalar bigger than 0'); end
+	if ((NindCh/SUBPOP) != np.fix(NindCh/SUBPOP)).all():
+		raise disagree('Chrom and SUBPOP disagree')
+	Nind = NindCh/SUBPOP  #Compute number of individuals per subpopulation
 
-    # Compute number of new individuals (to select)
-    NSel = max(np.floor(Nind*GGAP+0.5),2)
-    
-    # Select individuals from population
-    SelCh = None
-    for irun in range(SUBPOP):
-        FitnVSub = FitnV[int((irun)*Nind):int((irun+1)*Nind)]
-        ChrIx = SEL_F(FitnVSub, NSel)+(irun*Nind)
-        ChrIx = ChrIx.astype('int')
-        
-        if type(SelCh) != np.matrixlib.defmatrix.matrix:
-            SelCh = Chrom[ChrIx]
-        else:
-            SelCh = np.append(SelCh,Chrom[ChrIx],axis=0)
-            
-    return SelCh
+	# Compute number of new individuals (to select)
+	NSel = max(np.floor(Nind*GGAP+0.5),2)
+	
+	# Select individuals from population
+	SelCh = None
+	for irun in range(SUBPOP):
+		FitnVSub = FitnV[int((irun)*Nind):int((irun+1)*Nind)]
+		ChrIx = SEL_F(FitnVSub, NSel)+(irun*Nind)
+		ChrIx = ChrIx.astype('int')
+		
+		SelCh = SelCh and np.append(SelCh,Chrom[ChrIx],axis=0) or Chrom[ChrIx]
+			
+return SelCh
