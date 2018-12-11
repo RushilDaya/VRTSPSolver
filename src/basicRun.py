28240162675b216164dcd7793c72ba825a12123a
Rushil Daya <rushildayabme@gmail.com>
2018-12-11 11:50:42 +0100

    changed names

--- src/basicRun.py
+++ src/basicRun.py
@@ -4,24 +4,20 @@
 
 import pickle
 import numpy as np
-from run_ga import run_ga
-from xalt_edges import xalt_edges
+from tsp_runGA import tsp_runGA
+from tsp_xaltEdges import tsp_xaltEdges
 
 # every 
 
 def load_data(fileName):
     # dummy function for now
-    x = np.array([273.46509 367.34973 378.83914 225.10242 179.65437 272.98428 20.70829  31.18826 441.09842 215.28487 220.84784 153.61868 251.23114 339.36380 141.00000 143.00000])
-    y = np.array([[204.95761, 5.14920, 86.38704, 170.73392 ,211.91306, 97.19602 , 227.85622, 25.77590, 162.63313, 30.03942, 112.75235, 31.05856, 240.80257, 209.36363,143.00000,85.00000])
-
-
-    x = np.array([2,3,5,1,7,8,3,6,1,0,4,1])
-    y = np.array([2,3,2,7,2,8,9,4,1,1,2,8])
+    x = np.array([273.46509, 367.34973, 378.83914, 225.10242, 179.65437, 272.98428, 20.70829,  31.18826, 441.09842, 215.28487, 220.84784, 153.61868, 251.23114, 339.36380, 141.00000, 143.00000])
+    y = np.array([204.95761, 5.14920, 86.38704, 170.73392 ,211.91306, 97.19602 , 227.85622, 25.77590, 162.63313, 30.03942, 112.75235, 31.05856, 240.80257, 209.36363,143.00000,85.00000])
     return x,y
 
 def load_configurations(fileName):
     # dummy function for now
-    return [{'NAME':'conf1','NIND':50,'MAXGEN':500,'ELITIST':0.5,'STOP_PERCENTAGE':1,'PR_CROSS':0.3,'PR_MUT':0.1,'LOCALLOOP':None,'CROSSOVER':xalt_edges}]
+    return [{'NAME':'conf1','NIND':50,'MAXGEN':100,'ELITIST':0.0,'STOP_PERCENTAGE':1,'PR_CROSS':0.95,'PR_MUT':0.05,'LOCALLOOP':None,'CROSSOVER':tsp_xaltEdges}]
 
 def runConfiguration(config,x,y, dataFile ,runsNum):
     print(config)
@@ -40,7 +36,7 @@ def runConfiguration(config,x,y, dataFile ,runsNum):
     runs = []
 
     for i in range(runsNum):
-        runData = run_ga(x,y, config['NIND'], config['MAXGEN'], len(x),
+        runData = tsp_runGA(x,y, config['NIND'], config['MAXGEN'], len(x),
                          config['ELITIST'], config['STOP_PERCENTAGE'], config['PR_CROSS'],
                          config['PR_MUT'], config['CROSSOVER'], config['LOCALLOOP'])
         runs.append(runData)
