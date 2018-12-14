57091b88ab41bbf964efd26064eefc9f34671b43
Victor Jose Garcia Ferrnandez <victorjgf@gmail.com>
2018-12-14 20:37:56 +0100

    Tunned stop conditions

--- src/tsp_stopCriteria.py
+++ src/tsp_stopCriteria.py
@@ -4,7 +4,7 @@ THRESHOLD = float('Inf')
 #THRESHOLD = (10 ** -5)
 
 def mapping():
-	function_mappings = {'runingMean': generic_runingMean,'kImprovement': generic_kImprovement,'bestWorst': generic_bestWorst,'stdDev': generic_stdDev,'phi': generic_phi,'dummy': dummy}
+	function_mappings = {'runingMean': generic_runingMean,'bestWorst': generic_bestWorst,'stdDev': generic_stdDev,'phi': generic_phi,'dummy': dummy}
 	return function_mappings
 
 def setThreshold(sThreshold):
@@ -24,16 +24,7 @@ def generic_runingMean(scArgs):
 
 def runingMean(bestList, depth):
 	res = (len(bestList)<depth) and float('Inf') or abs(bestList[-depth:][-1]-np.mean(bestList[-depth:]))
-	return res<(THRESHOLD*0.00001)
-
-
-def generic_kImprovement(scArgs):
-	bestList,depth,sObjV,stopN = scArgs
-	return kImprovement(bestList, depth)
-
-def kImprovement(bestList, depth):
-	res = (len(bestList)<depth) and float('Inf') or all(bestList[-depth:][i] >= bestList[-depth:][i+1] for i in range(len(bestList[-depth:])-1))
-	return res<THRESHOLD
+	return res<=0
 
 ## Partial Convergence
 def generic_bestWorst(scArgs):
@@ -42,7 +33,7 @@ def generic_bestWorst(scArgs):
 
 def bestWorst(sObjV, stopN):
 	res =  (stopN > len(sObjV)) and float('Inf') or abs(sObjV[0]-sObjV[stopN])
-	return res<THRESHOLD
+	return res<=THRESHOLD
 
 
 def generic_stdDev(scArgs):
@@ -51,7 +42,7 @@ def generic_stdDev(scArgs):
 
 def stdDev(sObjV):
 	res = np.std(sObjV)
-	return res<THRESHOLD
+	return res<=THRESHOLD
 
 
 def generic_phi(scArgs):
@@ -60,7 +51,7 @@ def generic_phi(scArgs):
 
 def phi(sObjV):
 	res = sObjV[0]/np.mean(sObjV)
-	return res<THRESHOLD
+	return (1-res)<=THRESHOLD
 
 def dummy(Dummy):
 	return False
