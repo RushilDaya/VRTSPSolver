8b5dc71aa1e1edb6a126e66d0ee7ba4086bc5024
Victor Jose Garcia Ferrnandez <victorjgf@gmail.com>
2019-01-07 08:04:19 +0100

    Stop criteria implementation (stdDev)

--- src/tsp_stopCriteria.py
+++ src/tsp_stopCriteria.py
@@ -1,61 +1,41 @@
 import numpy as np
 
-#THRESHOLD = float('Inf')
-THRESHOLD = 0#(10 ** -5)
-
 def mapping():
-	function_mappings = {'tsp_runingMean': tsp_generic_runingMean,'tsp_bestWorst': tsp_generic_bestWorst,'tsp_stdDev': tsp_generic_stdDev,'tsp_phi': tsp_generic_phi,'tsp_none': tsp_none}
+	function_mappings = {'tsp_runingMean': tsp_runingMean,'tsp_bestWorst': tsp_bestWorst,'tsp_stdDev': tsp_stdDev,'tsp_phi': tsp_phi,'tsp_none': tsp_none}
 	return function_mappings
-
-def setThreshold(sThreshold):
-	global THRESHOLD
-	THRESHOLD = sThreshold
-
-def getThreshold():
-	return THRESHOLD
 
 ## Fully Convergence
 #def kIterations(MAX_ITERATIONS,N):
 #	return MAX_ITERATIONS<N
 
-def tsp_generic_runingMean(scArgs):
-	bestList,depth,sObjV,stopN = scArgs
-	return tsp_runingMean(bestList, depth)
+def tsp_genericStopCriteria(scArgs):
+	STOPCRITERIA,THRESHOLD,bestList,depth,sObjV,stopN,GEN,MAXGEN = scArgs
+	minGen = 50
+	if not(GEN < int(minGen)):
+		return STOPCRITERIA(THRESHOLD,bestList,depth,sObjV,stopN,GEN)
+	else:
+		False
 
-def tsp_runingMean(bestList, depth):
-	res = float('Inf')
-	if not(len(bestList)<depth):
-		res = abs(bestList[-depth:][-1]-np.mean(bestList[-depth:]))
-	return res<=THRESHOLD
+def tsp_runingMean(THRESHOLD,bestList,depth,sObjV,stopN,GEN):
+	if not(depth>=len(bestList)):
+		return (abs(bestList[-depth:][-1]/np.mean(bestList[-depth:]))<=THRESHOLD)
+	else:
+		return False
 
 ## Partial Convergence
-def tsp_generic_bestWorst(scArgs):
-	bestList,depth,sObjV,stopN = scArgs
-	return tsp_bestWorst(sObjV, stopN)
-
-def tsp_bestWorst(sObjV, stopN):
-	res = float('Inf') 
+def tsp_bestWorst(THRESHOLD,bestList,depth,sObjV,stopN,GEN):
 	if not((stopN > len(sObjV))):
-		res = abs(sObjV[0]-sObjV[stopN])
+		return abs(sObjV[0]-sObjV[stopN])<=THRESHOLD
+	else:
+		return False
+
+def tsp_stdDev(THRESHOLD,bestList,depth,sObjV,stopN,GEN):
+	res = np.std(bestList[-depth:])
 	return res<=THRESHOLD
 
-
-def tsp_generic_stdDev(scArgs):
-	bestList,depth,sObjV,stopN = scArgs
-	return tsp_stdDev(sObjV)
-
-def tsp_stdDev(sObjV):
-	res = np.std(sObjV)
-	return res<=THRESHOLD
-
-
-def tsp_generic_phi(scArgs):
-	bestList,depth,sObjV,stopN = scArgs
-	return tsp_phi(sObjV)
-
-def tsp_phi(sObjV):
+def tsp_phi(THRESHOLD,bestList,depth,sObjV,stopN,GEN):
 	res = sObjV[0]/np.mean(sObjV)
-	return ((1-res)<=THRESHOLD)
+	return (1-res<=THRESHOLD)
 
-def tsp_none(Dummy):
+def tsp_none(*args):
 	return False
